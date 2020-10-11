32ac301eebafb9bcfeb4d198b03ef43fddb59b09
taoer <312793211@qq.com>
2020-10-11 10:48:11 +0800

    实现了tabbar的基本功能，活跃状态的功能还有待修改

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="tab-bar-item" @click="onClick">
+    <div v-if="isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBarItem",
+  data:{
+    isActive:true
+  },
+  methods: {
+    onClick() {
+
+    },
+  },
+};
+</script>
+<style>
+#tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+#tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
